6930ad23e1ceb9d0fee2f708441597a864266d0e
Scott Iveland <scott.iveland@smartrg.com>
2023-01-11 13:34:12 -0800

    OWRT-7441 fix upgrade issue when upgrading from an older image without layout information

--- mediatek/image/smartrg.mk
+++ mediatek/image/smartrg.mk
@@ -138,6 +138,7 @@ define Build/srgImageRun
 	$(CP) $(TARGET_DIR)/usr/srg/scripts/img.sh $(KDIR)/img/scripts/
 	$(CP) $(TARGET_DIR)/usr/srg/scripts/flash-manage.sh $(KDIR)/img/scripts/
 	$(CP) $(TARGET_DIR)/usr/srg/scripts/emmc-manage.sh $(KDIR)/img/scripts/
+	$(CP) $(TARGET_DIR)/usr/srg/scripts/emmc-disk-* $(KDIR)/img/scripts/
 	$(CP) $(TARGET_DIR)/usr/srg/scripts/console_manage.sh $(KDIR)/img/scripts/
 	$(CP) $(TARGET_DIR)/etc/openwrt_release $(KDIR)/img/etc/
 	$(CP) $(KDIR)/root.squashfs.run.bin $(KDIR)/img/root.squashfs.bin
