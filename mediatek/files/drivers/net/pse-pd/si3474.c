595d983bd1009a32e53f0244b3dd6e70a7f3c353
Piotr Kubik <piotr.kubik@adtran.com>
2025-05-02 12:00:17 +0200

    SDG-3764: Follow kernel data types naming

--- mediatek/files/drivers/net/pse-pd/si3474.c
+++ mediatek/files/drivers/net/pse-pd/si3474.c
@@ -47,7 +47,7 @@
 #define SI3474_UV_STEP (1000 * 1000 * 60 / 16384)
 
 struct si3474_port_desc {
-	uint8_t chan[2];
+	u8 chan[2];
 	bool is_4p;
 };
 
@@ -68,9 +68,9 @@ static int si3474_pi_get_admin_state(struct pse_controller_dev *pcdev, int id,
 {
 	struct si3474_priv *priv = to_si3474_priv(pcdev);
 	struct i2c_client *client = priv->client;
-	bool enabled = FALSE;
-	uint8_t chan0, chan1;
-	int32_t ret;
+	bool enabled = false;
+	u8 chan0, chan1;
+	s32 ret;
 
 	ret = i2c_smbus_read_byte_data(client, POWER_STATUS_REG);
 	if (ret < 0) {
@@ -100,9 +100,9 @@ static int si3474_pi_get_pw_status(struct pse_controller_dev *pcdev, int id,
 {
 	struct si3474_priv *priv = to_si3474_priv(pcdev);
 	struct i2c_client *client = priv->client;
-	bool delivering = FALSE;
-	uint8_t chan0, chan1;
-	int32_t ret;
+	bool delivering = false;
+	u8 chan0, chan1;
+	s32 ret;
 
 	ret = i2c_smbus_read_byte_data(client, POWER_STATUS_REG);
 	if (ret < 0) {
@@ -130,20 +130,18 @@ static int si3474_get_of_channels(struct si3474_priv *priv)
 {
 	struct device_node *pse_node, *node;
 	struct pse_pi *pi;
-	uint32_t port_no, chan_id;
-	int8_t pairset_cnt;
-	int32_t ret = 0;
+	u32 port_no, chan_id;
+	s8 pairset_cnt;
+	s32 ret = 0;
 
 	pse_node = of_get_child_by_name(priv->np, "pse-pis");
 	if (!pse_node) {
-		dev_warn(
-		    &priv->client->dev,
-		    "Unable to parse DT PSE port-matrix, no pse-pis node\n");
+		dev_warn(&priv->client->dev,
+			 "Unable to parse DT PSE port-matrix, no pse-pis node\n");
 		return -EINVAL;
 	}
 
-	for_each_child_of_node(pse_node, node)
-	{
+	for_each_child_of_node(pse_node, node) {
 		if (!of_node_name_eq(node, "pse-pi"))
 			continue;
 
@@ -202,9 +200,8 @@ static int si3474_get_of_channels(struct si3474_priv *priv)
 			ret = of_property_read_u32(pi->pairset[1].np, "reg",
 						   &chan_id);
 			if (ret) {
-				dev_err(
-				    &priv->client->dev,
-				    "Failed to read channel reg property\n");
+				dev_err(&priv->client->dev,
+					"Failed to read channel reg property\n");
 				ret = -EINVAL;
 				goto out;
 			}
@@ -212,8 +209,7 @@ static int si3474_get_of_channels(struct si3474_priv *priv)
 			priv->port[port_no].is_4p = TRUE;
 		} else {
 			dev_err(&priv->client->dev,
-				"Number of pairsets incorrect - only 4p "
-				"configurations supported\n");
+				"Number of pairsets incorrect - only 4p configurations supported\n");
 			ret = -EINVAL;
 			goto out;
 		}
@@ -228,7 +224,7 @@ out:
 static int si3474_setup_pi_matrix(struct pse_controller_dev *pcdev)
 {
 	struct si3474_priv *priv = to_si3474_priv(pcdev);
-	int32_t ret;
+	s32 ret;
 
 	ret = si3474_get_of_channels(priv);
 	if (ret < 0) {
@@ -242,9 +238,9 @@ static int si3474_pi_enable(struct pse_controller_dev *pcdev, int id)
 {
 	struct si3474_priv *priv = to_si3474_priv(pcdev);
 	struct i2c_client *client = priv->client;
-	uint8_t chan0, chan1;
-	uint16_t val = 0;
-	int32_t ret;
+	u8 chan0, chan1;
+	u16 val = 0;
+	s32 ret;
 
 	if (id >= SI3474_MAX_CHANS)
 		return -ERANGE;
@@ -268,9 +264,9 @@ static int si3474_pi_disable(struct pse_controller_dev *pcdev, int id)
 {
 	struct si3474_priv *priv = to_si3474_priv(pcdev);
 	struct i2c_client *client = priv->client;
-	uint8_t chan0, chan1;
-	uint16_t val = 0;
-	int32_t ret;
+	u8 chan0, chan1;
+	u16 val = 0;
+	s32 ret;
 
 	if (id >= SI3474_MAX_CHANS)
 		return -ERANGE;
@@ -294,9 +290,9 @@ static int si3474_pi_is_enabled(struct pse_controller_dev *pcdev, int id)
 {
 	struct si3474_priv *priv = to_si3474_priv(pcdev);
 	struct i2c_client *client = priv->client;
-	bool enabled = FALSE;
-	uint8_t chan0, chan1;
-	int32_t ret;
+	bool enabled = false;
+	u8 chan0, chan1;
+	s32 ret;
 
 	ret = i2c_smbus_read_byte_data(client, POWER_STATUS_REG);
 	if (ret < 0)
@@ -315,9 +311,9 @@ static int si3474_pi_is_enabled(struct pse_controller_dev *pcdev, int id)
 static int si3474_pi_get_chan_current(struct si3474_priv *priv, u8 chan)
 {
 	struct i2c_client *client = priv->client;
-	int32_t ret;
-	uint8_t reg;
-	uint64_t tmp_64;
+	s32 ret;
+	u8 reg;
+	u64 tmp_64;
 
 	/* Registers 0x30 to 0x3d */
 	reg = PORT1_CURRENT_LSB_REG + (chan % 4) * 4;
@@ -336,9 +332,9 @@ static int si3474_pi_get_chan_current(struct si3474_priv *priv, u8 chan)
 static int si3474_pi_get_chan_voltage(struct si3474_priv *priv, u8 chan)
 {
 	struct i2c_client *client = priv->client;
-	int32_t ret;
-	uint8_t reg;
-	uint32_t val;
+	s32 ret;
+	u8 reg;
+	u32 val;
 
 	/* Registers 0x32 to 0x3f */
 	reg = PORT1_VOLTAGE_LSB_REG + (chan % 4) * 4;
@@ -357,8 +353,8 @@ static int si3474_pi_get_voltage(struct pse_controller_dev *pcdev, int id)
 	struct si3474_priv *priv = to_si3474_priv(pcdev);
 	struct i2c_client *client = priv->client;
 
-	uint8_t chan0, chan1;
-	int32_t ret;
+	u8 chan0, chan1;
+	s32 ret;
 
 	chan0 = priv->port[id].chan[0];
 	chan1 = priv->port[id].chan[1];
@@ -383,10 +379,10 @@ static int si3474_pi_get_voltage(struct pse_controller_dev *pcdev, int id)
 static int si3474_pi_get_actual_pw(struct pse_controller_dev *pcdev, int id)
 {
 	struct si3474_priv *priv = to_si3474_priv(pcdev);
-	int32_t ret;
-	uint32_t uV, uA;
-	uint64_t tmp_64;
-	uint8_t chan0, chan1;
+	s32 ret;
+	u32 uV, uA;
+	u64 tmp_64;
+	u8 chan0, chan1;
 
 	ret = si3474_pi_get_voltage(&priv->pcdev, id);
 	if (ret < 0)
@@ -413,22 +409,22 @@ static int si3474_pi_get_actual_pw(struct pse_controller_dev *pcdev, int id)
 }
 
 static const struct pse_controller_ops si3474_ops = {
-    .setup_pi_matrix = si3474_setup_pi_matrix,
-    .pi_enable = si3474_pi_enable,
-    .pi_disable = si3474_pi_disable,
-    .pi_get_actual_pw = si3474_pi_get_actual_pw,
-    .pi_get_voltage = si3474_pi_get_voltage,
-    .pi_is_enabled = si3474_pi_is_enabled,
-    .pi_get_admin_state = si3474_pi_get_admin_state,
-    .pi_get_pw_status = si3474_pi_get_pw_status,
+	.setup_pi_matrix = si3474_setup_pi_matrix,
+	.pi_enable = si3474_pi_enable,
+	.pi_disable = si3474_pi_disable,
+	.pi_get_actual_pw = si3474_pi_get_actual_pw,
+	.pi_get_voltage = si3474_pi_get_voltage,
+	.pi_is_enabled = si3474_pi_is_enabled,
+	.pi_get_admin_state = si3474_pi_get_admin_state,
+	.pi_get_pw_status = si3474_pi_get_pw_status,
 };
 
 static int si3474_i2c_probe(struct i2c_client *client)
 {
 	struct device *dev = &client->dev;
 	struct si3474_priv *priv;
-	int32_t ret;
-	uint8_t fw_version;
+	s32 ret;
+	u8 fw_version;
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
 		dev_err(dev, "i2c check functionality failed\n");
@@ -449,7 +445,7 @@ static int si3474_i2c_probe(struct i2c_client *client)
 	}
 
 	ret = i2c_smbus_read_byte_data(client, FIRMWARE_REVISION_REG);
-	if (ret  < 0)
+	if (ret < 0)
 		return ret;
 	fw_version = ret;
 
@@ -457,7 +453,8 @@ static int si3474_i2c_probe(struct i2c_client *client)
 	if (ret < 0)
 		return ret;
 
-	dev_info(&client->dev, "Chip revision: 0x%x, firmware version: 0x%x\n", ret, fw_version);
+	dev_info(&client->dev, "Chip revision: 0x%x, firmware version: 0x%x\n",
+		 ret, fw_version);
 
 	priv->client = client;
 	i2c_set_clientdata(client, priv);
@@ -481,20 +478,19 @@ static const struct i2c_device_id si3474_id[] = {{"si3474"}, {}};
 MODULE_DEVICE_TABLE(i2c, si3474_id);
 
 static const struct of_device_id si3474_of_match[] = {
-    {
-	.compatible = "skyworks,si3474",
-    },
-    {},
+	{
+		.compatible = "skyworks,si3474",
+	},
+	{},
 };
 MODULE_DEVICE_TABLE(of, si3474_of_match);
 
 static struct i2c_driver si3474_driver = {
-    .probe = si3474_i2c_probe,
-    .id_table = si3474_id,
-    .driver =
-	{
-	    .name = "si3474",
-	    .of_match_table = si3474_of_match,
+	.probe = si3474_i2c_probe,
+	.id_table = si3474_id,
+	.driver = {
+		.name = "si3474",
+		.of_match_table = si3474_of_match,
 	},
 };
 module_i2c_driver(si3474_driver);
